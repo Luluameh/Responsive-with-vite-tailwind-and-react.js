adf35a220d78121f508c8a6cb24ebfa78ec3093f
Luluameh <amehajumi3@gmail.com>
2023-04-20 04:36:48 +0100

    responsive

--- src/Container/Button.jsx
+++ src/Container/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({ link, Plus }) => {
   return (
-    <button className="border border-dashed  border- bg-[#F2F4F5] ml-2  p-2 w-72 rounded-lg ">
+    <button className="border border-dashed  border- bg-[#F2F4F5] ml-2  p-2 w-72 rounded-lg l:w-72 xl:w-80 xxl:w-[26rem] ">
       <div className=" flex items-center m-auto justify-center gap-3">
         <img src={Plus} alt="" />
         <a href="/" className=" text-[#7D8088] text-xs">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
     <div className="w-full 
     ">
       <Features />
-      <div className="flex flex-col lg:flex-row l:ml-[20%] xl:ml-[20%] xxl:gap-24">
+      <div className="flex flex-col lg:flex-row l:ml-[20%] xl:ml-[20%] xxl:">
       <div className="w-full lg:w-1/2 flex flex-col items-start gap-4 lg:gap-1">
 
           {/**pic card */}
